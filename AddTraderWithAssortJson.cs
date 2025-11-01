50587ecaac3fc2253a1dc5c9051f89cc0934bb03
silviohmartins <silviohmartins@gmail.com>
2025-11-01 14:25:55 -0300

    Adicionado novos presets, configurado lealdade e adequação da versão do SPT para 4.0.3

--- AddTraderWithAssortJson.cs
+++ AddTraderWithAssortJson.cs
@@ -19,8 +19,8 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Trader Jeronimo";
     public override string Author { get; init; } = "jero";
     public override List<string>? Contributors { get; init; } = [""];
-    public override SemanticVersioning.Version Version { get; init; } = new("1.1.0");
-    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.2");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.2.0");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.3");
     public override List<string>? Incompatibilities { get; init; } = [""];
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     public override string? Url { get; init; } = "";
