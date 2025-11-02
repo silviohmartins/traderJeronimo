f8529411b9ea3ca48b563782e3e91b242a1a7020
silviohmartins <silviohmartins@gmail.com>
2025-11-02 17:07:53 -0300

    ajuste de versao

--- AddTraderWithAssortJson.cs
+++ AddTraderWithAssortJson.cs
@@ -19,7 +19,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Trader Jeronimo";
     public override string Author { get; init; } = "jero";
     public override List<string>? Contributors { get; init; } = [""];
-    public override SemanticVersioning.Version Version { get; init; } = new("1.2.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.2.1");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.3");
     public override List<string>? Incompatibilities { get; init; } = [""];
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
